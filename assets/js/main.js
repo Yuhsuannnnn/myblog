e2b99926dfd05087cbfc447b1ceb1d2d8222c868
kmes41024 <kmes41024@gmail.com>
2022-08-06 17:04:14 +0800

    First commit

---
+++ assets/js/main.js
@@ -0,0 +1,24 @@
+var toTopBtn = document.getElementById('toTopBtn')
+toTopBtn.onclick = () => {
+    location.href="#"
+}
+
+var academicBtn = document.getElementById('academicBtn')
+academicBtn.onclick = () => {
+    location.href="academic.html"
+}
+
+var rewardBtn = document.getElementById('rewardBtn')
+rewardBtn.onclick = () => {
+    location.href="rewards.html"
+}
+
+var jobBtn = document.getElementById('jobBtn')
+jobBtn.onclick = () => {
+    location.href="internship.html"
+}
+
+var activityBtn = document.getElementById('activityBtn')
+activityBtn.onclick = () => {
+    location.href="activity.html"
+}
